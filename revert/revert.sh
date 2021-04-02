585e5485be32069e0896fe5efccf309302535f16
Andrew Cain <macite@gmail.com>
2021-04-02 14:42:12 +1100

    FIX: Use named branch in git checkout

--- revert/revert.sh
+++ revert/revert.sh
@@ -6,7 +6,7 @@ APP_PATH=`cd "$APP_PATH"; pwd`
 if [ "$(($1))" -eq 1 ]; then
     cd "$APP_PATH"
     git fetch --unshallow
-    git checkout 576f7038e6c1a53b07c8b27cb7911c2271d30c87
+    git checkout pre-fs-use
 else
     "$APP_PATH/help.sh"
     exit 1
